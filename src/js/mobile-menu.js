bfef75060443f956fbaea70f27b36e71d3f8cc9c
DagmaraSobczak <daga.sobczak@gmail.com>
2023-11-06 21:34:53 +0100

    mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,14 @@
-const menuOverlay = document.querySelector('.mobile-menu');
-const closeMenuButton = document.querySelector('.mobile-menu-btn');
-const openMenuBtn = document.querySelector('.mobile-menu__button');
+const mobileMenuButton = document.querySelector('[backdrop-open]');
+const mobileMenu = document.getElementById('mobile-menu');
+const backdrop = document.querySelector('[backdrop]');
 
-openMenuBtn.addEventListener('click', e => {
-  menuOverlay.classList.remove('d-none');
+mobileMenuButton.addEventListener('click', () => {
+  backdrop.classList.toggle('is-hidden');
+  mobileMenu.classList.toggle('d-none');
 });
 
-closeMenuButton.addEventListener('click', e => {
-  menuOverlay.classList.add('d-none');
+const closeButton = mobileMenu.querySelector('.mobile-menu-btn');
+closeButton.addEventListener('click', () => {
+  mobileMenu.classList.add('d-none');
+  backdrop.classList.add('is-hidden');
 });

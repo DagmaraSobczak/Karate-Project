7c3efa71b58a3dd50e9ae42b5535ee32576f7932
DagmaraSobczak <113237182+DagmaraSobczak@users.noreply.github.com>
2023-11-25 21:42:04 +0000

    Deploying to gh-pages from @ DagmaraSobczak/Karate-Project@25fb3553499a960ae164290a310626cf8c2716d3 🚀

---
+++ index.76f4ae9d.js
@@ -0,0 +1,3 @@
+document.querySelector(".baner-btn").addEventListener("click",function(){document.getElementById("form").scrollIntoView({behavior:"smooth"})});//# sourceMappingURL=index.76f4ae9d.js.map
+
+//# sourceMappingURL=index.76f4ae9d.js.map

a7685a3f8fb9cb7ddbe8066f6a50753762e85688
DagmaraSobczak <113237182+DagmaraSobczak@users.noreply.github.com>
2023-12-03 23:15:43 +0000

    Deploying to gh-pages from @ DagmaraSobczak/Karate-Project@d6d2a3afb7342b5dc1064aeca0c49305853f7339 🚀

---
+++ index.cada0f38.js
@@ -0,0 +1,3 @@
+const t=document.querySelector('.nav__link[href="#Kontakt"]'),e="/index.html"===window.location.pathname;e?t.addEventListener("click",function(t){t.preventDefault();let e=document.getElementById("Kontakt");e&&e.scrollIntoView({behavior:"smooth"})}):t.setAttribute("href","/index.html#Kontakt");//# sourceMappingURL=index.cada0f38.js.map
+
+//# sourceMappingURL=index.cada0f38.js.map
---
+++ zajęcia.537e9a8e.js
@@ -0,0 +1,3 @@
+new SimpleLightbox(".gallery a",{captionSrc:"alt",captionsData:"alt",captionDelay:250});//# sourceMappingURL=zajęcia.537e9a8e.js.map
+
+//# sourceMappingURL=zajęcia.537e9a8e.js.map

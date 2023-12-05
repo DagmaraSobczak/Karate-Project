983b62b1cc75ac3d2ff221780ba261b79ba92cf4
DagmaraSobczak <113237182+DagmaraSobczak@users.noreply.github.com>
2023-12-05 21:26:54 +0000

    Deploying to gh-pages from @ DagmaraSobczak/Karate-Project@4b35ac4f6483d1a2750034f935878594f2e34260 🚀

---
+++ sztukiwalki.3900452c.js
@@ -0,0 +1,3 @@
+new SimpleLightbox(".gallery a",{captionSrc:"alt",captionsData:"alt",captionDelay:250});//# sourceMappingURL=sztukiwalki.3900452c.js.map
+
+//# sourceMappingURL=sztukiwalki.3900452c.js.map

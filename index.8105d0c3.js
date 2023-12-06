699fa99ef87b0b984f57486596ab24edc71542e9
DagmaraSobczak <113237182+DagmaraSobczak@users.noreply.github.com>
2023-12-06 21:22:44 +0000

    Deploying to gh-pages from @ DagmaraSobczak/Karate-Project@5a8a75248916696235add2e9ae5aa34d1106e9d9 🚀

---
+++ index.8105d0c3.js
@@ -0,0 +1,3 @@
+document.querySelectorAll(".mobile-menu__link--dropdown").forEach(e=>{e.addEventListener("click",function(e){e.preventDefault();let t=this.getAttribute("href"),o="/sztukiwalki.html"===window.location.pathname;if(o){let e=document.querySelector(t);e&&e.scrollIntoView({behavior:"smooth"})}else window.location.href="/sztukiwalki.html"+t})});//# sourceMappingURL=index.8105d0c3.js.map
+
+//# sourceMappingURL=index.8105d0c3.js.map

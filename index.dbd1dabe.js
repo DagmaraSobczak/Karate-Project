f7941b9acf30e8eaac115d3c0dd72dced5d5c1c0
DagmaraSobczak <113237182+DagmaraSobczak@users.noreply.github.com>
2023-12-11 22:22:20 +0000

    Deploying to gh-pages from @ DagmaraSobczak/Karate-Project@01990042d0b50092c617cb3d88856e25a288223b 🚀

--- index.dbd1dabe.js
+++
@@ -1,3 +0,0 @@
-const e=document.querySelector("[backdrop-open]"),t=document.getElementById("mobile-menu"),d=document.querySelector("[backdrop]");e.addEventListener("click",()=>{d.classList.toggle("is-hidden"),t.classList.toggle("d-none")});const n=t.querySelector(".mobile-menu-btn");n.addEventListener("click",()=>{t.classList.add("d-none"),d.classList.add("is-hidden")});//# sourceMappingURL=index.dbd1dabe.js.map
-
-//# sourceMappingURL=index.dbd1dabe.js.map
---
+++ index.ee7f91bd.js
@@ -0,0 +1,4 @@
+const e=document.querySelector("[backdrop-open]");document.getElementById("mobile-menu");const d=document.querySelector("[backdrop]");let t=!1;// Zmienna śledząca stan menu
+e.addEventListener("click",()=>{t?(d.classList.add("is-hidden"),t=!1):(d.classList.remove("is-hidden"),t=!0)});//# sourceMappingURL=index.ee7f91bd.js.map
+
+//# sourceMappingURL=index.ee7f91bd.js.map

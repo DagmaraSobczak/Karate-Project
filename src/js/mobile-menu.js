01990042d0b50092c617cb3d88856e25a288223b
DagmaraSobczak <daga.sobczak@gmail.com>
2023-12-11 23:21:43 +0100

    mobile-menu,js poprawka

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,13 +2,16 @@ const mobileMenuButton = document.querySelector('[backdrop-open]');
 const mobileMenu = document.getElementById('mobile-menu');
 const backdrop = document.querySelector('[backdrop]');
 
-mobileMenuButton.addEventListener('click', () => {
-  backdrop.classList.toggle('is-hidden');
-  mobileMenu.classList.toggle('d-none');
-});
+let isMenuOpen = false; // Zmienna śledząca stan menu
 
-const closeButton = mobileMenu.querySelector('.mobile-menu-btn');
-closeButton.addEventListener('click', () => {
-  mobileMenu.classList.add('d-none');
-  backdrop.classList.add('is-hidden');
+mobileMenuButton.addEventListener('click', () => {
+  if (!isMenuOpen) {
+    backdrop.classList.remove('is-hidden');
+
+    isMenuOpen = true;
+  } else {
+    backdrop.classList.add('is-hidden');
+
+    isMenuOpen = false;
+  }
 });
